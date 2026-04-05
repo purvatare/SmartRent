1d16b18a358f393ff438fc981f54970f3924322b
purvatare <160874476+purvatare@users.noreply.github.com>
2026-04-06 00:09:02 +0530

    Create Agreement button added

--- block.js
+++ block.js
@@ -1,65 +1,171 @@
 // block.js
-
+import { rentChainBytecode } from "./bytecode.js";
 let web3;
 let userAccount;
-const contractAddress = "0x6287305E85d3D6ae8a45fF3cC447bC25A69dc44A";
+const contractAddress = "0x4dd9ef7f82be202e9d54e0d004799a12c26f5114";
 
 // Simple ABI for your contract
 const abi = [
-  {
-    "constant": false,
-    "inputs": [],
-    "name": "payRent",
-    "outputs": [],
-    "payable": true,
-    "stateMutability": "payable",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [],
-    "name": "terminateAgreement",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [],
-    "name": "returnSecurityDeposit",
-    "outputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getDueAmount",
-    "outputs": [{ "name": "", "type": "uint256" }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "isRentLate",
-    "outputs": [{ "name": "", "type": "bool" }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "isActive",
-    "outputs": [{ "name": "", "type": "bool" }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  }
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_tenant",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_rentAmount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_deposit",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "dueDate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getDueAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isActive",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isRentLate",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "landlord",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lastPaidDate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "payRent",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rentAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "returnSecurityDeposit",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "securityDeposit",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "tenant",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "terminateAgreement",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+
 ];
 
 let contract;
@@ -143,7 +249,69 @@ async function returnSecurityDeposit() {
     alert("Error: " + err.message);
   }
 }
+async function createAgreement() {
+  console.log("Create clicked");
 
+  try {
+    if (!window.ethereum) return alert("Install MetaMask");
+
+    // Connect wallet
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+
+    const web3 = new Web3(window.ethereum);
+    const accounts = await web3.eth.getAccounts();
+    const user = accounts[0];
+
+    console.log("User:", user);
+
+    // Take inputs
+    const tenant = prompt("Enter Tenant Address:");
+    const rent = prompt("Enter Rent in ETH (e.g. 0.01):");
+    const deposit = prompt("Enter Deposit in ETH (e.g. 0.05):");
+
+    if (!tenant || !rent || !deposit) {
+      return alert("All fields are required!");
+    }
+
+    // Convert ETH → Wei
+    const rentWei = web3.utils.toWei(rent, "ether");
+    const depositWei = web3.utils.toWei(deposit, "ether");
+
+    console.log("Tenant:", tenant);
+    console.log("Rent (Wei):", rentWei);
+    console.log("Deposit (Wei):", depositWei);
+
+    // Create contract instance (NO address here!)
+    const contract = new web3.eth.Contract(abi);
+
+    const bytecode = rentChainBytecode;
+
+    // Deploy contract
+    const gasEstimate = await contract.deploy({
+      data: bytecode,
+      arguments: [tenant, rentWei, depositWei]
+    }).estimateGas({ from: user });
+
+    console.log("Gas Estimate:", gasEstimate);
+
+    const newContract = await contract.deploy({
+      data: bytecode,
+      arguments: [tenant, rentWei, depositWei]
+    }).send({
+      from: user,
+      gas: gasEstimate + 500000   // 🔥 buffer
+    });
+
+    console.log("New Contract:", newContract.options.address);
+
+    alert("✅ New Agreement Created!");
+    alert("Contract Address: " + newContract.options.address);
+
+  } catch (err) {
+    console.error(err);
+    alert("Error: " + (err.message || err));
+  }
+}
 const gasEstimate = await contract.methods.payRent().estimateGas({
   from: userAccount,
   value: due
